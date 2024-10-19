8f7629c97cdbd5f83fd27d0e1995856b1c6afbba
Skylar Ennenga <skylar.ennenga@gmail.com>
2024-10-19 12:41:24 -0600

    Dsiplay Critic Score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,19 @@
+import { Badge } from "@chakra-ui/react";
+
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+
+    let color = score > 75 ? "green" : score > 60 ? "yellow" : score > 50 ? "orange" : "red";
+
+  return (
+    <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius={4}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
